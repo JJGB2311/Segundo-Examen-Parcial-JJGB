5ba1d10770a63f3a2f6254ed5786b7d51317ebac
JJGB2311 <jgamezb@miumg.edu.gt>
2020-04-29 10:01:04 -0600

    MDI Contenedor 2do parcial

---
+++ 2 Parcial Juan Jose Gamez Blanco 0901-16-47/2 Parcial Juan Jose Gamez Blanco/2do Parcial.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace _2_Parcial_Juan_Jose_Gamez_Blanco
+{
+    partial class _2do_Parcial
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(_2do_Parcial));
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.inicioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manteniemientoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.areaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.procesosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.seguridadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.despuesDeLaCuarentenaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.juanJoseGamezBlanco09011947ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.LblUsuario = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.menuStrip.SuspendLayout();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.inicioToolStripMenuItem,
+            this.manteniemientoToolStripMenuItem,
+            this.procesosToolStripMenuItem,
+            this.seguridadToolStripMenuItem,
+            this.ayudaToolStripMenuItem,
+            this.juanJoseGamezBlanco09011947ToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(948, 28);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "MenuStrip";
+            // 
+            // inicioToolStripMenuItem
+            // 
+            this.inicioToolStripMenuItem.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inicioToolStripMenuItem.Name = "inicioToolStripMenuItem";
+            this.inicioToolStripMenuItem.Size = new System.Drawing.Size(61, 24);
+            this.inicioToolStripMenuItem.Text = "Inicio";
+            // 
+            // manteniemientoToolStripMenuItem
+            // 
+            this.manteniemientoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.areaToolStripMenuItem});
+            this.manteniemientoToolStripMenuItem.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.manteniemientoToolStripMenuItem.Name = "manteniemientoToolStripMenuItem";
+            this.manteniemientoToolStripMenuItem.Size = new System.Drawing.Size(146, 24);
+            this.manteniemientoToolStripMenuItem.Text = "Manteniemiento ";
+            // 
+            // areaToolStripMenuItem
+            // 
+            this.areaToolStripMenuItem.Name = "areaToolStripMenuItem";
+            this.areaToolStripMenuItem.Size = new System.Drawing.Size(114, 24);
+            this.areaToolStripMenuItem.Text = "Area";
+            this.areaToolStripMenuItem.Click += new System.EventHandler(this.areaToolStripMenuItem_Click);
+            // 
+            // procesosToolStripMenuItem
+            // 
+            this.procesosToolStripMenuItem.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.procesosToolStripMenuItem.Name = "procesosToolStripMenuItem";
+            this.procesosToolStripMenuItem.Size = new System.Drawing.Size(87, 24);
+            this.procesosToolStripMenuItem.Text = "Procesos";
+            // 
+            // seguridadToolStripMenuItem
+            // 
+            this.seguridadToolStripMenuItem.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.seguridadToolStripMenuItem.Name = "seguridadToolStripMenuItem";
+            this.seguridadToolStripMenuItem.Size = new System.Drawing.Size(95, 24);
+            this.seguridadToolStripMenuItem.Text = "Seguridad";
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.despuesDeLaCuarentenaToolStripMenuItem});
+            this.ayudaToolStripMenuItem.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(69, 24);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // despuesDeLaCuarentenaToolStripMenuItem
+            // 
+            this.despuesDeLaCuarentenaToolStripMenuItem.Name = "despuesDeLaCuarentenaToolStripMenuItem";
+            this.despuesDeLaCuarentenaToolStripMenuItem.Size = new System.Drawing.Size(272, 24);
+            this.despuesDeLaCuarentenaToolStripMenuItem.Text = "Despues de la cuarentena";
+            // 
+            // juanJoseGamezBlanco09011947ToolStripMenuItem
+            // 
+            this.juanJoseGamezBlanco09011947ToolStripMenuItem.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.juanJoseGamezBlanco09011947ToolStripMenuItem.Name = "juanJoseGamezBlanco09011947ToolStripMenuItem";
+            this.juanJoseGamezBlanco09011947ToolStripMenuItem.Size = new System.Drawing.Size(282, 24);
+            this.juanJoseGamezBlanco09011947ToolStripMenuItem.Text = "Juan Jose Gamez Blanco 0901-19-47";
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.BackColor = System.Drawing.Color.DodgerBlue;
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel,
+            this.LblUsuario});
+            this.statusStrip.Location = new System.Drawing.Point(0, 547);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(948, 25);
+            this.statusStrip.TabIndex = 2;
+            this.statusStrip.Text = "StatusStrip";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(58, 20);
+            this.toolStripStatusLabel.Text = "Estado";
+            // 
+            // LblUsuario
+            // 
+            this.LblUsuario.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblUsuario.Name = "LblUsuario";
+            this.LblUsuario.Size = new System.Drawing.Size(21, 20);
+            this.LblUsuario.Text = "...";
+            // 
+            // _2do_Parcial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(948, 572);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.menuStrip);
+            this.DoubleBuffered = true;
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "_2do_Parcial";
+            this.Text = "2Do Parcial JJGB";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this._2do_Parcial_Load);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.ToolTip toolTip;
+        private System.Windows.Forms.ToolStripMenuItem inicioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manteniemientoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem procesosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem seguridadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem despuesDeLaCuarentenaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem juanJoseGamezBlanco09011947ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripStatusLabel LblUsuario;
+        private System.Windows.Forms.ToolStripMenuItem areaToolStripMenuItem;
+    }
+}
+
+
+
---
+++ 2 Parcial Juan Jose Gamez Blanco 0901-16-47/2 Parcial Juan Jose Gamez Blanco/Area.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2_Parcial_Juan_Jose_Gamez_Blanco
+{
+    public partial class Area : Form
+    {
+        string usuario;
+        public Area(string user)
+        {
+            InitializeComponent();
+            usuario = user;
+            string[] alias = { "ID","Nombre", "Estado" }; // Arreglo de nombres para campos
+            navegador1.asignarAlias(alias); // Asignar nombres
+            navegador1.asignarSalida(this); // Asignar form de salida
+            Color nuevoColor = System.Drawing.ColorTranslator.FromHtml("#0926E3"); // Deficion de 
+            navegador1.asignarColorFondo(nuevoColor);
+            navegador1.asignarColorFuente(Color.Black);
+            navegador1.asignarAyuda("1"); // asignar 1 por defecto 
+                                          // LOS COMBOS SE ASIGNAN SEGUN EL ORDEN EN QUE SE DECLAREN
+          //  navegador1.asignarComboConTabla("tabla", "campo", 0); // 0 o 1 en modo, 0 pone el id y 1 coloca el nombre y consulta el id
+            navegador1.asignarTabla("area"); // tabla principal
+            navegador1.asignarNombreForm("Area"); // Titulo y nombre del form
+        }
+
+        private void Area_Load(object sender, EventArgs e)
+        {
+            string aplicacionActiva = "1";
+            navegador1.ObtenerIdUsuario(usuario); // Pasa el parametro del usuario
+            navegador1.botonesYPermisosInicial(usuario, aplicacionActiva); // Consulta permisos al iniciar
+            navegador1.ObtenerIdAplicacion(aplicacionActiva);// Pasa el id de la aplicacion actual
+        }
+    }
+}
